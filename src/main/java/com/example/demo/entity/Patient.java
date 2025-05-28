e9f42fae59e7dee39abdf4230a074abd84475293
DevMorningCoffee <132598665+DEVMorningCoffee@users.noreply.github.com>
2025-05-27 20:10:33 -0400

    Create Patient entity and configure mapping to Neon PostgreSQL database

---
+++ src/main/java/com/example/demo/entity/Patient.java
@@ -0,0 +1,54 @@
+package com.example.demo.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Table(name = "patient")
+@Entity(name = "patient")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Patient {
+
+    public enum Gender{
+        MALE,
+        FEMALE,
+        OTHER
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID patientId;
+
+    @Column(name = "first_name", nullable = false, length = 50)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 50)
+    private String lastName;
+
+    @Column(name = "date_of_birth", nullable = false)
+    @JsonFormat(pattern = "mm-dd-yyyy")
+    private LocalDate dateOfBirth;
+
+    @Column(name = "gender", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @Column(name = "username", nullable = false, unique = true, length = 25)
+    private String username;
+
+    @Column(name = "password", nullable = false, length = 35)
+    private String password;
+
+    @Column(name = "contact_info")
+    private String contactInfo;
+
+    @Column(name = "medical_info")
+    private String medicalInfo;
+}
