bf0037b065ccae4fa981407e68c6a53090cd1bd6
DevMorningCoffee <132598665+DEVMorningCoffee@users.noreply.github.com>
2025-05-27 22:46:51 -0400

    Implement @ControllerAdvice to handle patient-related exceptions globally

--- src/main/java/com/example/demo/exception/controller/PatientExceptionController.java
+++ src/main/java/com/example/demo/exception/controller/PatientExceptionController.java
@@ -1,4 +1,37 @@
 package com.example.demo.exception.controller;
 
+import com.example.demo.exception.DuplicatePatientException;
+import com.example.demo.exception.ErrorResponse;
+import com.example.demo.exception.InvalidInputException;
+import com.example.demo.exception.MissingFieldException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class PatientExceptionController {
+    @ExceptionHandler(value = DuplicatePatientException.class)
+    public ResponseEntity<ErrorResponse> handleDuplicatePatientException(DuplicatePatientException e) {
+        ErrorResponse errorResponse = new ErrorResponse("Duplicate Patient", e.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(value = InvalidInputException.class)
+    public ResponseEntity<Object> handleInvalidInputException(InvalidInputException e) {
+        ErrorResponse errorResponse = new ErrorResponse("Invalid Input", e.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = MissingFieldException.class)
+    public ResponseEntity<Object> handleMissingFieldException(MissingFieldException e) {
+        ErrorResponse errorResponse = new ErrorResponse("Missing Input", e.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGenericException(Exception e) {
+        ErrorResponse errorResponse = new ErrorResponse("InternalError", "An unexpected error occurred");
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
